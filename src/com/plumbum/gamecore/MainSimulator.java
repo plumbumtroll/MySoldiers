50b3da8a59f900fb7500e7367b1943d936d4b394
DeHj <>
2022-09-16 20:15:07 +0300

    храним ссылку на поведение в классе Warrior

--- src/com/plumbum/gamecore/MainSimulator.java
+++ src/com/plumbum/gamecore/MainSimulator.java
@@ -20,31 +20,34 @@ public class MainSimulator {
                 map[x][y] = new Cell();
             }
         }
-        population = new Warrior[commandSize * 2];
-        for (int i = 0; i < (commandSize - 1); i++) {
-            population[i] = new Warrior ('H');
-        }
-        for (int i = commandSize; i < (commandSize * 2 - 1); i++) {
-            population[i] = new Warrior ('R');
-        }
 
-        firstAI = new StupidBot();
-        secondAI = new StupidBot();
+        IBot firstAI = new StupidBot();
+        IBot secondAI = new StupidBot();
+
+        population = new Warrior[commandSize * 2];
+        for (int i = 0; i < commandSize; i++) {
+            population[i] = new Warrior ('H', firstAI);
+        }
+        for (int i = commandSize; i < commandSize * 2; i++) {
+            population[i] = new Warrior ('R', secondAI);
+        }
     }
 
     public void displaySimulation () {
         System.out.println(Arrays.toString(map));
 
     }
+
     public void startSimulation(int duration) {
 
         Point з = new Point(1, 2);
 
         for (int i = 0; i < duration; i++) {
 
-            for (int j = 0; j < firstCommand.length; j++) {
+            for (int j = 0; j < population.length; j++) {
 
-                int result = firstAI.toDoMove(map);
+                IBot behaviour = population[j].getBehaviour();
+                int result = behaviour.toDoMove(map);
 
                 switch (result) {
                     case (0):
@@ -57,25 +60,7 @@ public class MainSimulator {
                         // и т. д.
                         break;
                 }
-
             }
-
-            for (int j = 0; j < secondCommand.length; j++) {
-
-                int result = secondAI.toDoMove(map);
-                switch (result) {
-                    case (0):
-                        // логика обработки того, как солдатик стоит на месте
-                        break;
-                    case (1):
-                        // и т. д.
-                        break;
-                    case (2):
-                        // и т. д.
-                        break;
-                }
-            }
-
         }
     }
 }
--- src/com/plumbum/gamecore/Warrior.java
+++ src/com/plumbum/gamecore/Warrior.java
@@ -2,9 +2,14 @@ package com.plumbum.gamecore;
 
 public class Warrior {
     private char commandSymbol;
+    private IBot behaviour;
 
-    public Warrior(char commandSymbol) {
+    public Warrior(char commandSymbol, IBot behaviour) {
         this.commandSymbol = commandSymbol;
+        this.behaviour = behaviour;
     }
 
+    public IBot getBehaviour(){
+        return behaviour;
+    }
 }
