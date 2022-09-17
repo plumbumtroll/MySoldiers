93b71b05eb29881554f73321daef1be5893e7556
plumbumtroll <speacenows@mail.ru>
2022-09-17 13:07:57 +0300

    Поправили Cell

--- src/com/plumbum/MySoldiers.java
+++ src/com/plumbum/MySoldiers.java
@@ -1,16 +1,19 @@
 package com.plumbum;
 
 import com.plumbum.gamecore.MainSimulator;
+import com.plumbum.gamecore.Warrior;
+import com.plumbum.gamersai.StupidBot;
 
 import java.sql.Array;
 
 public class MySoldiers {
     public static void main(String[] args){
-        MainSimulator newWorld = new MainSimulator();
+        MainSimulator newWorld = new MainSimulator(3, 3, 3, 2);
 //        newWorld.startSimulation(20);
 
         System.out.println("abyrvalg");
-        newWorld.displaySimulation();
+        Warrior StupidDestroyer = new Warrior('A', new StupidBot());
 
+        newWorld.displaySimulation();
     }
 }
