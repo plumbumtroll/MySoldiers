dafcc5e27104f1b87bb6ef864048ac5b659bb14f
DeHj <>
2022-09-17 23:07:28 +0300

    Простейшее поведение у StupidBot - бродит в случайную сторону

--- src/com/plumbum/gamersai/StupidBot.java
+++ src/com/plumbum/gamersai/StupidBot.java
@@ -3,10 +3,20 @@ package com.plumbum.gamersai;
 import com.plumbum.gamecore.Cell;
 import com.plumbum.gamecore.IBot;
 import com.plumbum.gamecore.actions.Action;
+import com.plumbum.gamecore.actions.Movement;
+
+import java.util.Random;
 
 public class StupidBot implements IBot {
+
+    Random random;
+
+    public StupidBot(){
+        random = new Random();
+    }
+
     @Override
     public Action toDoMove(Cell[][] information) {
-        return null;
+        return new Movement(random.nextInt( 3) - 1, random.nextInt( 3) - 1);
     }
 }
