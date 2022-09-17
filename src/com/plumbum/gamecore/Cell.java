248a2f891bfc4837546f07e1e1f3414a1af573ff
plumbumtroll <speacenows@mail.ru>
2022-09-17 13:10:43 +0300

    Перегрузили метод toString

--- src/com/plumbum/gamecore/Cell.java
+++ src/com/plumbum/gamecore/Cell.java
@@ -3,6 +3,15 @@ package com.plumbum.gamecore;
 public class Cell {
     private Warrior localWarrior;
 
+    @Override
+    public String toString() {
+        if (localWarrior == null) {
+            return "*";
+        } else {
+            return String.valueOf(localWarrior.commandSymbol);
+        }
+    }
+
     public Cell(){
 
     }
