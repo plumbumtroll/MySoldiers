42e7cf4cead0f8dff3041c448c2500027487ee04
plumbumtroll <speacenows@mail.ru>
2022-09-17 13:05:43 +0300

    Поправили Cell

--- src/com/plumbum/gamersai/StupidBot.java
+++ src/com/plumbum/gamersai/StupidBot.java
@@ -1,10 +1,11 @@
 package com.plumbum.gamersai;
 
+import com.plumbum.gamecore.Cell;
 import com.plumbum.gamecore.IBot;
 
 public class StupidBot implements IBot {
     @Override
-    public int toDoMove(int[][] information) {
+    public int toDoMove(Cell[][] information) {
         return 0;
     }
 }
